c08ff95c7b5d181f471b722366a2bd63fe97511a
sereja-s <serejasuvorov1979@gmail.com>
2023-03-11 22:16:41 +0300

    каталог

--- views/page/catalog.php
+++ views/page/catalog.php
@@ -10,19 +10,18 @@ $this->title = 'Каталог';
 
 
 <div class="col-lg-3 col-md-3 col-sm-5 col-xs-12 left_banner_menu">
-  баннерное меню
+	<h4>Каталог товаров -></h4>
 </div>
 
 <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 catalog">
-  
-    <div class="row content">
-        <?php foreach ($categories as $category):?>
-            <div class="col-lg-4 col-md-6 col-sm-4 col-xs-12 catalog_category">
-                <a href="<?=Url::toRoute(['page/listproducts', 'id' => $category['id']])?>"><img src="images/<?= $category['img'];?>"></a>
-                <a href="<?=Url::toRoute(['page/listproducts', 'id' => $category['id']])?>"><?= $category['name'];?></a>
-            </div>
-        <?php endforeach;?>
-             
-    </div>
-</div>
 
+	<div class="row content">
+		<?php foreach ($categories as $category) : ?>
+			<div class="col-lg-4 col-md-6 col-sm-4 col-xs-12 catalog_category">
+				<a href="<?= Url::toRoute(['page/listproducts', 'id' => $category['id']]) ?>"><img src="images/<?= $category['img']; ?>"></a>
+				<a href="<?= Url::toRoute(['page/listproducts', 'id' => $category['id']]) ?>"><?= $category['name']; ?></a>
+			</div>
+		<?php endforeach; ?>
+
+	</div>
+</div>
